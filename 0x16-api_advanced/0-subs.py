5b25ad9415082746d40861d1953609a9716538de
sylvan452 <sylvan452@gmail.com>
2023-06-06 12:33:43 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        "User-Agent": 'Google Chrome Version 114.0.5735.110'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
